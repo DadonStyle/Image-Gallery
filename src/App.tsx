58e0341ae68a866472db17c86ed147474a67232f
DadonStyle <noamoni9@gmail.com>
2025-02-27 07:49:04 +0200

    add react scan for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import Gallery from './modules/Gallery/Gallery';
-import AnimatedCursorComponent from './components/AnimatedMouse/AnimatedMouse';
-import { QueryClientProvider, QueryClient } from 'react-query';
-import useInitialConsoleLog from './hooks/useInitialConsoleLog';
+import { scan } from "react-scan";
+import Gallery from "./modules/Gallery/Gallery";
+import AnimatedCursorComponent from "./components/AnimatedMouse/AnimatedMouse";
+import { QueryClientProvider, QueryClient } from "react-query";
+import useInitialConsoleLog from "./hooks/useInitialConsoleLog";
+
+scan({
+  enabled: false, // in production set to false
+});
 
 function App() {
   useInitialConsoleLog();
